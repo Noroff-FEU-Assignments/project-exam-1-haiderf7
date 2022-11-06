8d88c325c75ff6b25ad3479f9f83c1af4b160d17
haiderf7 <91637854+haiderf7@users.noreply.github.com>
2022-11-06 16:46:50 +0100

    n

---
+++ js/subscribe.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("#subscribeForm");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+
+function validateForm(event) {
+    event.preventDefault();
+
+  
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+
+    
+
+    console.log("hello");
+};
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
